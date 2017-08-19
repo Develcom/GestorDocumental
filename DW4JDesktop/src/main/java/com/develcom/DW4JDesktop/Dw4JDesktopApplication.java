da22291b38c5f5268db354b70cf3b79177bd57a8
Erick Escalona <erickescalona@gmail.com>
2017-08-19 13:05:06 -0400

    DW4J Desktop, la aplicacion de escritorio del gestor documental

---
+++ DW4JDesktop/src/main/java/com/develcom/DW4JDesktop/Dw4JDesktopApplication.java
@@ -0,0 +1,12 @@
+package com.develcom.DW4JDesktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dw4JDesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dw4JDesktopApplication.class, args);
+	}
+}
